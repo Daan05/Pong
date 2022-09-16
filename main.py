cc324e503b86c8ddeebe1e5cc072620e4f55723e
Daan05 <d.h.valkema@gmail.com>
2022-09-16 20:29:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-main():
-     print("Hello, World")
+import pygame, sys
+pygame.init()
+
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+screen = pygame.display.set_mode((800, 500))
+
+def main():
+     quit = False
+
+     # game loop
+     while not quit:
+
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT: sys.exit()
+               
      return
-
-
-     
 
 if __name__ == "__main__":
      main()
